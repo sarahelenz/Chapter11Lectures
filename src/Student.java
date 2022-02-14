30759e0b0a4d445bf1796c89af81820bb2e2ba43
sarahelenz <selenz3112@stu.d214.org>
2022-02-14 13:02:43 -0600

    added setters and getters to Student.java

--- src/Student.java
+++ src/Student.java
@@ -6,4 +6,20 @@ public class Student {
     public String toString(){
         return name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIDNumber() {
+        return IDNumber;
+    }
+
+    public void setIDNumber(int IDNumber) {
+        this.IDNumber = IDNumber;
+    }
 }
